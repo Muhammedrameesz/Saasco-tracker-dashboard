7e2698fee1bd933f214419d3f06f619fe91d969b
Muhammedrameesz <rameezta555@gmail.com>
2025-06-20 22:22:47 +0530

    new changes

--- src/api/const.ts
+++ src/api/const.ts
@@ -1,3 +1,3 @@
 export const baseUrl = "https://trackingapp-backend-fksa.onrender.com/api/v1"
 
-export const localUrl ="http://localhost:7000/api/v1"
+export const localUrl ="https://trackingapp-backend-fksa.onrender.com/api/v1"
