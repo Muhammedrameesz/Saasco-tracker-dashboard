2b8ce3fb0191583d871b22005e4ef0c2a3b2d4f1
Muhammedrameesz <rameezta555@gmail.com>
2025-08-20 08:20:13 +0530

    updated

--- src/api/const.ts
+++ src/api/const.ts
@@ -1,6 +1,6 @@
-export const baseUrl = "https://tracking.magicmusicuae.com:7001/api/v1"
+export const baseUrl = "https://tracking.magicmusicuae.com:7000/api/v1"
 
-export const localUrl ="http://localhost:7001/api/v1"
+export const localUrl ="https://tracking.magicmusicuae.com:7000/api/v1"
 
 // http://localhost:7000/api/v1
 
