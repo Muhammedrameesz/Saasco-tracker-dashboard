72af4bcf4b4485d5d3f6d3a1ed06e26ac943bb11
Muhammedrameesz <rameezta555@gmail.com>
2025-08-24 21:14:51 +0530

    api endpoint updated

--- src/api/const.ts
+++ src/api/const.ts
@@ -1,6 +1,6 @@
 export const baseUrl = "https://tracking.magicmusicuae.com:7000/api/v1"
 
-export const LocalUrl ="https://trackingapp-backend-fksa.onrender.com/api/v1"
+export const LocalUrl ="https://tracking.magicmusicuae.com:7000/api/v1"
 
 // http://localhost:7000/api/v1
 
