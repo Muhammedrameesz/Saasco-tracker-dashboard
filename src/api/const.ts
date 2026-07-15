32f90353cc026f522eaf8ff599629f32863c6272
Ramees <ramees@Rameess-MacBook-Air-2.local>
2026-07-15 15:44:45 +0530

    new

--- src/api/const.ts
+++ src/api/const.ts
@@ -1,6 +1,6 @@
-const isProduction =false;
+const isProduction =true;
 
-const PROD_URL = "https://tracking.magicmusicuae.com:7000/api/v1";
+const PROD_URL = "https://saasco-tracking.onrender.com/api/v1";
 const DEV_URL = "http://localhost:7001/api/v1";
 
 export const baseUrl = isProduction ? PROD_URL : DEV_URL;
@@ -10,7 +10,7 @@ export const LocalUrl = isProduction ? PROD_URL : DEV_URL;
 // http://localhost:7000/api/v1
 //  "https://backend.magicmusicuae.com/api/v1";
 
-// https://tracking.magicmusicuae.com:7000/api/v1
+// https://tracking.magicmusicuae.com:7000/api/v1  this one final
 
 // https://trackingapp-backend-fksa.onrender.com/api/v1
 
